3960edd7b20da0a97d8c9fa63e1b5ae65082645e
omerDemirel <demirel999@yahoo.com>
2023-02-24 09:44:29 +0100

    initial commit

---
+++ src/main/java/com/ea/SpringBasic/libraries/WebDriverLibrary.java
@@ -0,0 +1,42 @@
+package com.ea.SpringBasic.libraries;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+@Profile("!remote")
+public class WebDriverLibrary {
+
+    @Bean
+    @ConditionalOnProperty(name = "browser", havingValue = "chrome")
+    public WebDriver getChromeDriver() {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        WebDriverManager.chromedriver().setup();
+        return new ChromeDriver(chromeOptions);
+    }
+
+    @Bean
+    @Scope("browserscope")
+    @ConditionalOnProperty(name = "browser", havingValue = "firefox")
+    public WebDriver getFirefoxDriver() {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+
+        //chromeOptions.addArguments("--headless");
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver(firefoxOptions);
+    }
+
+
+}
